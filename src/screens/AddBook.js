55fd48a145d006570b08902edf874ad503a38d48
MacBook Pro <ghd8119@naver.com>
2021-02-23 21:27:53 +0900

    #4 ReviewPage - Searching new Books

--- src/screens/AddBook.js
+++ src/screens/AddBook.js
@@ -1,11 +1,54 @@
-import React, { useState }  from 'react';
+import React, { useState, useEffect }  from 'react';
 import {View, ScrollView, StyleSheet} from 'react-native';
-import { Button, Searchbar} from 'react-native-paper';
+import { Button, Searchbar, Text} from 'react-native-paper';
 import BookList from './BookList'
 
 const AddBook = ({navigation}) => {
     const [searchQuery, setSearchQuery] = React.useState('');
     const onChangeSearch = query => setSearchQuery(query);
+    let [loading, loadingModify] = React.useState({
+        isLoading : true,
+        books : []
+    });
+
+    let getBooks = async () => {
+        if(loading.isLoading == true){
+            fetch('http://localhost:8000/book/search/', {
+                method : 'POST',
+                headers: { 'Accept':'application/json', 'Content-Type': 'application/json' },
+                body : JSON.stringify({
+                    title : "안녕"
+                })
+            }).then(response => response.json() )
+            .then(async data =>{
+                loadingModify({books : data, isLoading:false})
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+        }
+    }
+
+    let searchBooks = async () => {
+        fetch('http://localhost:8000/book/search/', {
+            method : 'POST',
+            headers: { 'Accept':'application/json', 'Content-Type': 'application/json' },
+            body : JSON.stringify({
+                title : searchQuery
+            })
+        }).then(response => response.json() )
+        .then(async data =>{
+            loadingModify({books : data})
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+    }
+
+    useEffect(()=>{
+        getBooks();
+    });
+
     return (
         <View style={styles.container}>
             <View style={styles.makeRow}>
@@ -19,13 +62,29 @@ const AddBook = ({navigation}) => {
             <Button 
                 style={styles.searchBtn}
                 mode='contained'
+                onPress={searchBooks}
             >검색</Button> 
         </View>
         
         <ScrollView>
-            <BookList></BookList>
-            <BookList></BookList>
-            <BookList></BookList>
+            {
+                loading.isLoading
+                ? <Text>Loading ...</Text>
+                : (
+                    loading.books.map((book, idx) => {
+                        return(
+                            <BookList
+                                id = {idx}
+                                title = {book.title}
+                                authors = {book.authors}
+                                publisher = {book.publisher}
+                                grade = {book.grade}
+                                review = {book.review}
+                            />
+                        )
+                    })
+                )
+            }
         </ScrollView>
         </View>
     );
